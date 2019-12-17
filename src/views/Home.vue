b4424f3b8d1bcf54dc303b11e33f357680edb3a0
Seyed Mohammad Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2019-12-17 17:33:25 +0330

    Home route redundent component removed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,27 @@
 <template>
-  <div class="home">
-    <HomeContent></HomeContent>
-  </div>
+    <div>
+      <HomeHeader></HomeHeader>
+      <AboutEvent></AboutEvent>
+      <Workshops></Workshops>  
+      <AboutEvent></AboutEvent>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HomeContent from '../components/HomeContent.vue'
+// import HomeContent from '@/components/HomeContent.vue'
+
+import AboutEvent from "@/components/HomeComponents/AboutEvent";
+import Workshops from "@/components/HomeComponents/Workshops";
+import HomeHeader from "@/components/HomeComponents/HomeHeader";
 
 export default {
   name: 'home',
   components: {
-    HomeContent
+    HomeHeader, AboutEvent, Workshops
+  },
+  mounted(){
+    console.log("home route mounted.")
   }
 }
 </script>
