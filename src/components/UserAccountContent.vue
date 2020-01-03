3eb31833b4b60c3a1d52338a8d989fababe02bdb
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-01-04 01:47:08 +0330

    user account contents updated, restructured, api calls and loadings added

--- src/components/UserAccountContent.vue
+++ src/components/UserAccountContent.vue
@@ -1,66 +1,72 @@
 <template>
-    <div class="parent">
-        <div class="top">
-        </div>
-        <div class="bottom">
-            <div class="left info-card">
-                <div class="head">
-                    <div class="line"></div>
-                    <div class="title"><h4> کارگاه ها </h4></div>
-                    <div class="line"></div>
+    <div class="infoWrapper">
+        <div class="infoMiddleWrapper">
+            
+            <div class="userInfoBox">
+                <div class="infoBoxHeader">
+                    <div class="infoBoxHeader-line"></div>
+                    <div class="infoBoxHeader-title"><h4> اطلاعات شخصی </h4></div>
+                    <div class="infoBoxHeader-line"></div>
                 </div>
-                <div class="workshop-list" v-for="workshop in workshops">
-                    <div class="workshop-item">
-                        <div class="sub two"><p>{{workshop.name}}</p></div>
-                        <div class="sub three" :key="workshop.teachers.indexOf(teacher)" v-for="teacher in workshop.teachers"><p>{{teacher}}</p></div>
-                        <div class="sub four"><p><span>{{workshop.price}}</span>هزار تومان</p></div>
-                        <div class="sub five">
-                            <router-link :to="'/workshops/' + workshop._id">اطلاعات بیشتر</router-link>
-                        </div>
-                    </div>
+                <div class="infoItem">
+                    <h3 class="infoItemTitle">نام و نام خانوادگی ؛</h3>
+                    <p class="infoItemContent">{{this.user.firstName + " " + this.user.lastName}}</p>
                 </div>
-            </div>
-            <div class="right workshop-card">
-                <div class="head">
-                    <div class="line"></div>
-                    <div class="title"><h4> اطلاعات شخصی </h4></div>
-                    <div class="line"></div>
+                <div class="infoItem">
+                    <h3 class="infoItemTitle">ایمیل :</h3>
+                    <p class="infoItemContent">{{this.user.email}}</p>
                 </div>
-                <div class="info-list">
-                    <p class="info-title">نام:</p>
-                    <p class="yellow">{{this.user.firstName + " " + this.user.lastName}}</p>
-                    <p class="info-title">ایمیل:</p>
-                    <p class="yellow">{{this.user.email}}</p>
-                    <p class="info-title">شماره تماس:</p>
-                    <p class="yellow">{{this.user.phoneNumber}}</p>
-                    <p class="info-title" v-if="this.user.studentNumber != undefined">شماره دانشجویی:</p>
-                    <p v-if="this.user.studentNumber != undefined" class="yellow">{{this.user.studentNumber}}
-                    <p class="info-title">سن:</p>
-                    <p class="yellow">{{this.user.age}}</p>
+                <div class="infoItem">
+                    <h3 class="infoItemTitle">‌‌شماره تماس :‌</h3>
+                    <p class="infoItemContent">{{this.user.phoneNumber}}</p>
                 </div>
-                <div class="button">
-                    <button>
-                        <router-link to="/user/edit" class="link">ویرایش اطلاعات</router-link>
-                    </button>
+                <div class="infoItem">
+                    <h3 class="infoItemTitle">سن :</h3>
+                    <p class="infoItemContent">{{this.user.age}}</p>
+                </div>
+                <div class="infoItem" v-if="user.studentNumbe != undefined">
+                    <h3 class="infoItemTitle">شماره دانشجویی :</h3>
+                    <p class="infoItemContent">{{this.user.studentNumber}}</p>
+                </div>
+                <div class="infoItem">
+                    <router-link class="infoItemButton" to="/user/edit">ویرایش اطلاعات</router-link>
                 </div>
             </div>
 
+            <div class="workshopsInfoBox">
+                <div class="infoBoxHeader">
+                    <div class="infoBoxHeader-line"></div>
+                    <div class="infoBoxHeader-title dark"><h4>لیست کارگاه های ثبت نام شده</h4></div>
+                    <div class="infoBoxHeader-line"></div>
+                </div>
+                <WorkshopDoneRegisterItem :key="workshop._id" v-for="workshop in workshops" :workshop="workshop"></WorkshopDoneRegisterItem>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import axios from 'axios'
+    import WorkshopDoneRegisterItem from '@/components/WorkshopComponents/WorkshopDoneRegisterItem'
     export default {
         name: "UserAccountContent",
+        components : {
+            WorkshopDoneRegisterItem
+        },
         data: function () {
             return {
                 user : {},
                 workshops : []
             }
         },
-        computed: {
+        methods: {
             getUser: function() {
+                    this.$notify({
+                        group : "auth",
+                        title : "صبر کنید",
+                        text : "چند لحظه صبر کنید...",
+                        type : "warn"
+                    })
                 axios({
                     url : this.$store.getters.baseUrl + '/users/me',
                     method : 'GET',
@@ -69,15 +75,26 @@
                     console.log(response);
                     this.user = response.data.user;
                     this.workshops = response.data.workshops;
+                    this.$notify({
+                                group: "auth",
+                                title: "موفقیت",
+                                text: "اطلاعات شما دریافت شد.",
+                                type: "success"
+                            });
                 }).catch(error => {
                     console.log(error.response);
+                            this.$notify({
+                                group: "auth",
+                                title: "خطا",
+                                text: "خطایی هنگام ارتباط با سرور رخ داد. لطفا اتصال اینترنت خود را بررسی کنید",
+                                type: "error"
+                            });
                 })
             },
         },
         created() {
             console.log("component created");
             this.getUser(); 
-            // this.$store.dispatch('getUserFromServer');
         },
         mounted() {
             console.log("component mounted.");
@@ -86,169 +103,114 @@
 </script>
 
 <style scoped>
-    p{
-        font-family: 'iransans';
-    }
-    h4{
-        font-family: 'iransans';
-    }
-    .parent {
+    .infoWrapper {
+        display:flex;
+        align-items:flex-start;
+        justify-content:center;
+        min-height:calc(100vh - 40px);
         background-color: #e8e8e8;
     }
 
-    .top img {
-        width: 100%;
+    .infoMiddleWrapper {
+        width:1140px;
+        display:flex;
+        align-items:flex-start;
+        justify-content:space-between;
+        margin-top:40px;
     }
 
-    .bottom {
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: center;
-        padding: 40px 0 30px 0;
-    }
-
-    .left {
-        min-height: 300px;
-        min-width: 50%;
-        border-radius: 30px;
-        background-color: white;
-        display: flex;
-        flex-direction: column;
-        padding: 10px 40px 10px 40px;
-
-    }
-
-    .right {
-        min-height: 300px;
-        min-width: 25%;
-        border-radius: 30px;
+    .userInfoBox {
         background-color: black;
+        border-radius:30px;
+        flex-flow: column wrap;
+        display:flex;
+        margin-left:20px;
+        padding:10px;
+    }
+
+    .infoItem {
+        padding-right:15px;
+        padding-left:15px;
+        margin-top:15px;
         display: flex;
         flex-direction: column;
-        color: white;
-        padding: 10px 20px 10px 20px;
-        text-align: right;
     }
 
-    .yellow {
-        color: #c7962b;
-        margin: 5px 30px 30px 0;
-    }
-
-    .right button {
-        background-color: #e4b22b;
-        border-radius: 25px;
-        border: none;
-        padding: 5px 10px 5px 10px;
-        font-size: 15px;
-        color: white;
-        margin: 20px 0 0 0;
+    .infoItemTitle {
+        color:white;
+        font-size:14px;
         font-family: 'iransans';
     }
 
-    .right button:hover{
-        opacity: 0.8;
-    }
-    .button {
-        text-align: left;
-    }
-
-
-
-    .left .line {
-        border-bottom: 3px solid #c7962b;
-        width: 100%;
-        height: 30px;
-        position: relative;
-        top: -20px;
-    }
-
-    .right .line {
-        border-bottom: 3px solid #c7962b;
-        width: 100%;
-        height: 30px;
-        position: relative;
-        top: -20px;
-    }
-
-    .title {
-        text-align: center;
-        padding: 0 auto;
-        margin: 0 10px 0 10px;
-        min-width: 100px;
+    .infoItemContent {
+        color: #e4b22b;
+        font-size:17px;
+        font-weight:bold;
+        margin-right:5px;
+        margin-top:5px;
         font-family: 'iransans';
     }
 
-    .head {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-    }
-
-    .workshop-list {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-
-    .workshop-item {
-        background-color: black;
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: space-between;
-        align-items: stretch;
-        border-radius: 25px;
-        width: 90%;
-        margin: 0 auto;
-        color: white;
-    }
-
-    .sub img {
-        width: 50px;
-        height: 100%;
-        border-radius: 0 25px 25px 0;
-    }
-
-    .one {
-        width: 50px;
-        border-left: 2px solid #e4b22b;
-    }
-
-    .workshop-item .sub button {
-        background-color: #e4b22b;
-        border-radius: 25px;
-        border: none;
-        padding: 5px 10px 5px 10px;
-        font-size: 15px;
-        color: white;
-        margin: 10px;
+    .infoItemButton {
+        margin:auto;
+        margin-bottom:10px;
         font-family: 'iransans';
-    }
-
-    .link {
         text-decoration: none;
-        color: white;
-        font-family: 'iransans';
+        color:white;
+        background-color: #e4b22b;
+        border-radius:15px;
+        padding:5px 25px;
     }
-    .info-title{
-        margin: 20px 30px 0 0;
+
+    .infoBoxHeader {
+        display:flex;
+        align-items:center;
+        justify-content:space-between;
+        margin-top:15px;
+        margin-right:10px;
+        margin-left:10px;
+    }
+
+    .infoBoxHeader-line {
+        height:4px;
+        background-color: #e4b22b;
+        justify-self: stretch;
+        width:100px;
+    }
+
+    .infoBoxHeader-title {
+        color:white;
+        font-weight:bold;
+        text-align:center;
         font-family: 'iransans';
     }
 
-    @media only screen and (max-width: 768px) {
-        p,h4{
-            font-size: smaller;
+    .workshopsInfoBox {
+        background-color: #f4f4f4;
+        border-radius:30px;
+        flex-flow: column wrap;
+        display:flex;
+        padding:10px;
+        width:calc(70% - 60px);
+    }
+
+    .infoBoxHeader-title.dark {
+        color: #333;
+    }
+
+    @media only screen and (max-width: 876px) {
+        .infoMiddleWrapper {
+            flex-direction:column;
+            align-items:stretch;            
         }
-        .bottom{
-            display: flex;
-            flex-direction: column-reverse;
-            justify-content: center;
-            align-items: center;
-            padding: 40px 0 30px 0;
-        }
-        .left , .right{
-            width: 80%;
-            margin: 5px;
+        .userInfoBox {
+            margin:10px;
+        }  
+
+        .workshopsInfoBox {
+            margin: 10px;
+            width: initial;
         }
     }
+    
 </style>
---
+++ src/components/WorkshopComponents/WorkshopDoneRegisterItem.vue
@@ -0,0 +1,148 @@
+<template>
+    <div class="workshopRegItemWrapper">
+        <div class="workshopRegItemContent">
+            <img class="workshopRegItemImage" draggable="false" src="../../assets/img/workshop.png" :alt="'workshop ' + workshop.id + ' image'">
+            <h3 class="workshopRegItemTitle">
+                <i class="material-icons rotate-180" >double_arrow</i>
+                {{workshop.title}}
+            </h3>
+            <h3 class="workshopRegItemPrice">
+                <i class="material-icons">attach_money</i>
+                 تومان {{workshop.price}}
+            </h3>
+            <router-link :to="'/workshops/' + workshop._id" class="workshopRegItemMoreButton">اطلاعات بیشتر...</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "WorkshopDoneRegisterItem",
+        props : {
+            workshop : {},
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .workshopRegItemWrapper {
+        display : flex;
+        align-items:center;
+        width:100%;
+        margin-top:5px;
+        margin-bottom:5px;
+    }
+
+    .workshopRegItemContent {
+        border-radius:30px;
+        background-color: #191919;
+        height:65px;
+        width:calc(100%);
+        display: flex;
+        align-items:center;
+        transition:all 0.2s ease-in;
+        cursor:default;
+    }
+
+    .workshopRegItemContent.selected {
+        background-color:#e4b22b;
+    }
+
+    .workshopRegItemImage {
+        height:65px;
+        border-radius:0 30px 30px 0;
+        border-left:3px solid #f9f9f9;
+    }
+    .workshopRegItemTitle {
+        font-family : 'iransans';
+        display:flex;
+        align-items:center;
+        color:white;
+        margin-left:auto;
+    }
+
+    .workshopRegItemTitle i.material-icons {
+        margin-right:15px;
+        margin-left:15px;
+    }
+
+    .workshopRegItemPrice {
+        font-family : 'iransans';
+        display:flex;
+        align-items:center;
+        color:white;
+    }
+
+    .material-icons.rotate-180 {
+        transform: rotate(180deg);
+    }
+
+    .workshopRegItemMoreButton {
+        margin-right:auto;
+        margin-left:20px;
+        background-color:#ddd;
+        color: #333;
+        text-decoration: none;
+        font-family : 'iransans';
+        padding:5px 10px;
+        border-radius:30px;
+    }
+
+    .workshopRegItemContent:hover {
+        background-color: #521c34;
+    }
+
+    .workshopRegItemContent:hover .workshopRegItemTitle,
+    .workshopRegItemContent:hover .workshopRegItemPrice {
+        color: #e4b22b;
+    }
+
+    .workshopRegItemContent:hover .workshopRegItemMoreButton {
+        background-color :#e4b22b;
+        color:white;
+    }
+
+    .workshopRegItemContent:hover .workshopRegItemImage {
+        border-left-color: #e4b22b;
+    }
+
+    i.material-icons.selectedIcon {
+        position: absolute;
+        color: rgba(255,255,255,0.8);
+        font-weight: bold;
+        font-size: 50px;
+        margin-right: 7px;
+    }
+
+    @media only screen and (max-width: 576px) {
+        .workshopRegItemTitle i.material-icons {
+            margin-right:5px;
+            margin-left:5px;
+            font-size:14px;
+        }
+
+        .workshopRegItemTitle {
+            font-size:14px;
+            font-weight:normal;
+        }
+
+        .workshopRegItemPrice {
+            font-size: 14px;
+        }
+
+        .workshopRegItemPrice i.material-icons {
+            font-size:14px;
+        }
+
+        .workshopRegItemMoreButton {
+            font-size: 10px;
+            font-weight: bold;
+        }
+    }
+
+</style>
