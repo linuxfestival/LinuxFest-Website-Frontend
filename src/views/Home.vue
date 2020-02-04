fa280bf8b3a09b453bdbbf064ee595bb758afcf9
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-02-05 01:19:00 +0330

    Home page components updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,28 @@
 <template>
     <div>
-      <HomeHeader></HomeHeader>
-      <AboutEvent></AboutEvent>
-      <Workshops></Workshops>  
-      <AboutEvent></AboutEvent>
+        <HomeHeader/>
+        <AboutEvent/>
+        <Workshops/>
+        <Sponsor />
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HomeContent from '@/components/HomeContent.vue'
+    // @ is an alias to /src
+    // import HomeContent from '@/components/HomeContent.vue'
 
-import AboutEvent from "@/components/HomeComponents/AboutEvent";
-import Workshops from "@/components/HomeComponents/Workshops";
-import HomeHeader from "@/components/HomeComponents/HomeHeader";
+    import AboutEvent from "@/components/HomeComponents/AboutEvent";
+    import Workshops from "@/components/HomeComponents/Workshops";
+    import HomeHeader from "@/components/HomeComponents/HomeHeader";
+    import Sponsor from "@/components/HomeComponents/Sponsor";
 
-export default {
-  name: 'home',
-  components: {
-    HomeHeader, AboutEvent, Workshops
-  },
-  mounted(){
-    console.log("home route mounted.")
-  }
-}
+    export default {
+        name: 'home',
+        components: {
+            HomeHeader, AboutEvent, Workshops, Sponsor
+        },
+        mounted() {
+            console.log("home route mounted.")
+        }
+    }
 </script>
