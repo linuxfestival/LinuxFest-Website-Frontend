e7edbd8f22671c455afd43ea33c682b97e57203a
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-02-05 01:19:33 +0330

    Main js entry point point updated to work with v-scroll library

--- src/main.js
+++ src/main.js
@@ -5,10 +5,27 @@ import store from './store/store'
 import VueWait from 'vue-wait'
 import Notifications from 'vue-notification'
 import Vuelidate from 'vuelidate'
-import VueRouter from 'vue-router'
-Vue.config.productionTip = false
-Vue.use(VueWait) // add VueWait as Vue plugin
-Vue.use(Notifications)
+import VueScrollTo from 'vue-scrollto';
+
+let vueScrollToConfig = {
+  container: "body",
+  duration: 500,
+  easing: "ease",
+  offset: -50,
+  force: true,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+};
+
+Vue.config.productionTip = false;
+
+Vue.use(VueScrollTo, vueScrollToConfig);
+Vue.use(VueWait);
+Vue.use(Notifications);
 Vue.use(Vuelidate);
 
 new Vue({
@@ -28,4 +45,4 @@ new Vue({
     directiveName: 'wait',       // <span v-wait /> directive name, you can set `my-loader` etc.
 
   }),
-}).$mount('#app')
+}).$mount('#app');
