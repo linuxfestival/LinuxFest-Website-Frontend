3210a11deecc6b2a9f5f34049cdd298a6fcbb4b2
Hadi <hadi.tabatabaei.aut@gmail.com>
2021-08-26 01:54:28 +0430

    style: Reformat code

--- src/views/Workshop/components/Description.vue
+++ src/views/Workshop/components/Description.vue
@@ -2,78 +2,77 @@
   <div class="workshopContentDescription">
     <h1 class="workshopContentDescription-title">
       <i class="material-icons rotate-180">double_arrow</i>
-      {{getTitle()}}
+      {{ getTitle() }}
     </h1>
-    <div
-      class="workshopContentDescription-desc"
-      v-html="description"
-    />
+    <div class="workshopContentDescription-desc" v-html="description" />
 
-    <h3 class="timeline-title">{{$t('workshop.description.timeTable')}}</h3>
+    <h3 class="timeline-title">{{ $t("workshop.description.timeTable") }}</h3>
 
     <p
       class="timeline-description"
       v-for="(timeSlot, index) in times"
-     :key="timeSlot.id"
+      :key="timeSlot.id"
     >
-      {{getTimeTableText(timeSlot, index)}}
+      {{ getTimeTableText(timeSlot, index) }}
     </p>
     <h3 class="timeline-title">
-      {{$t('workshop.description.registrationFee')}}
+      {{ $t("workshop.description.registrationFee") }}
     </h3>
     <p class="timeline-description">
-      {{this.getPriceText()}}
+      {{ this.getPriceText() }}
     </p>
     <RegisterButton :isRegOpen="isRegOpen" :id="registerButtonId" />
   </div>
 </template>
 
 <script>
-import { getTimeTable } from '../utils';
+import { getTimeTable } from "../utils";
 
-import RegisterButton from './RegisterButton.vue';
+import RegisterButton from "./RegisterButton.vue";
 
 export default {
-  name: 'Description',
+  name: "Description",
   components: {
-    RegisterButton
+    RegisterButton,
   },
   props: {
     price: Number,
     description: String,
-    isRegOpen: Boolean ,
+    isRegOpen: Boolean,
     times: Array,
     registerButtonId: String,
   },
   computed: {
     isFree() {
       return this.price === 0;
-    }
+    },
   },
   methods: {
     getTitle() {
-      if(this.isFree) {
-        return this.$t('workshop.description.aboutThisTalk')
+      if (this.isFree) {
+        return this.$t("workshop.description.aboutThisTalk");
       } else {
-        return this.$t('workshop.description.aboutThisWorkshop')
+        return this.$t("workshop.description.aboutThisWorkshop");
       }
     },
     getPriceText() {
-      if(this.isFree) {
-        return this.$t('workshop.description.free')
+      if (this.isFree) {
+        return this.$t("workshop.description.free");
       } else {
-        return this.$t('workshop.description.priceInRials', { price: this.price })
+        return this.$t("workshop.description.priceInRials", {
+          price: this.price,
+        });
       }
     },
     getTimeTableText(timeSlot, index) {
       return getTimeTable(timeSlot, index);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .workshopContentDescription {
+.workshopContentDescription {
   width: 680px;
   min-height: 400px;
   border-radius: 15px 0 0 15px;
@@ -85,7 +84,7 @@ export default {
 
 .workshopContentDescription-title {
   margin-top: 10px;
-  font-family: 'iransans';
+  font-family: "iransans";
   color: #e4b22b;
   display: flex;
   align-items: center;
@@ -96,7 +95,7 @@ export default {
 }
 
 .workshopContentDescription-desc {
-  font-family: 'iransans';
+  font-family: "iransans";
   color: white;
   text-align: justify;
   width: 95%;
@@ -104,19 +103,18 @@ export default {
 }
 
 .timeline-title {
-  font-family: 'iransans';
+  font-family: "iransans";
   color: #e4b22b;
   margin-right: 30px;
   margin-top: 10px;
 }
 
 .timeline-description {
-  font-family: 'iransans';
+  font-family: "iransans";
   color: white;
   font-size: 15px;
   margin: 5px 40px;
 }
-
 
 @media only screen and (max-width: 1100px) {
   .workshopContentDescription {
@@ -130,7 +128,7 @@ export default {
   }
 }
 
-@media only screen and (max-width: 500px ) {
+@media only screen and (max-width: 500px) {
   .workshopContentDescription-desc {
     margin-right: 5px;
   }
