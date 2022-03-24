6ffc860fb408a849f2d5a3ca8a536680b1021e6d
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2022-03-24 15:53:00 +0430

    fix: Remove Bozorgvar (the penguin in backgound)

--- src/views/Home/components/AboutEvent.vue
+++ src/views/Home/components/AboutEvent.vue
@@ -30,7 +30,8 @@ export default {
   align-items: center;
   justify-content: center;
   min-height: 500px;
-  background: #252040 url(../../../assets/img/pinguin.png) no-repeat left / 50%;
+  background: #252040 no-repeat left / 50%;
+  /*background: #252040 url(../../../assets/img/pinguin.png) no-repeat left / 50%;*/
 }
 
 .aboutEventContainer {
